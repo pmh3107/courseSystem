2f2a2a5ac997f57280140d7987cc252321466df7
Hiển Phan <87771924+pmh3107@users.noreply.github.com>
2024-08-10 17:08:45 +0700

    FEAT: demo scss file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+
 function App() {
 	return (
-		<>
-			<h1>Hello</h1>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+	return (
+		<div>
+			<h1>this is homepages</h1>
+		</div>
+	);
+}
